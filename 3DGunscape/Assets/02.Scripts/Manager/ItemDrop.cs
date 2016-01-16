51c91408f31b46f3f921c386147adfba491e5ecd
Jin Park <jiny199109@gmail.com>
2016-01-16 17:21:55 +0900

    Monster drops items

---
+++ 3DGunscape/Assets/02.Scripts/Manager/ItemDrop.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemDrop : MonoBehaviour
+{
+    public DropItemInfo[] Items;
+
+    // Use this for initialization
+	void Start ()
+	{
+        Debug.Log("Item : " + Items.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    [System.Serializable]
+    public struct DropItemInfo
+    {
+        public GameObject Item;
+        public float Chance;
+    }
+
+    public void Drop(Vector3 position)
+    {
+        float dropRate = Random.Range(0f, 1f);
+        foreach (DropItemInfo itemInfo in Items)
+        {
+            float chance = itemInfo.Chance;
+            Debug.Log("dropRate : " + dropRate + " chance : " + chance);
+            if (dropRate < chance)
+            {
+                Instantiate(itemInfo.Item, position, Quaternion.identity);
+                return;
+            }
+            dropRate -= chance;
+        }
+    }
+}
