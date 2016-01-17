bbea9a7614f752776d857806caaf946bcf280888
Jin Park <jiny199109@gmail.com>
2016-01-17 20:49:52 +0900

    Monster attacks

---
+++ 3DGunscape/Assets/02.Scripts/Control/MonsterAttack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterAttack : MonoBehaviour
+{
+    private Transform Target;
+    public float Damage;
+
+    // Use this for initialization
+    void Start()
+    {
+        Target = PlayerWeapons.player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AttackTarget()
+    {
+        Target.BroadcastMessage("ApplyDamage", Damage, SendMessageOptions.DontRequireReceiver);
+        Target.BroadcastMessage("Direction", transform, SendMessageOptions.DontRequireReceiver);
+    }
+}
