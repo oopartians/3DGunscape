ab0eff4106af192493af72c7bcc57955115a219c
Jin Park <jiny199109@gmail.com>
2016-01-17 21:03:13 +0900

    Able to dodge monster's attack.

--- 3DGunscape/Assets/02.Scripts/Control/MonsterAttack.cs
+++ 3DGunscape/Assets/02.Scripts/Control/MonsterAttack.cs
@@ -5,11 +5,13 @@ public class MonsterAttack : MonoBehaviour
 {
     private Transform Target;
     public float Damage;
+    private float AttackDistance;
 
     // Use this for initialization
     void Start()
     {
         Target = PlayerWeapons.player.transform;
+        AttackDistance = GetComponent<MonsterCtrl>().AttackDistance;
     }
 
     // Update is called once per frame
@@ -20,7 +22,12 @@ public class MonsterAttack : MonoBehaviour
 
     public void AttackTarget()
     {
-        Target.BroadcastMessage("ApplyDamage", Damage, SendMessageOptions.DontRequireReceiver);
-        Target.BroadcastMessage("Direction", transform, SendMessageOptions.DontRequireReceiver);
+        float distance = Vector3.Distance(Target.position, gameObject.transform.position);
+
+        if (distance < AttackDistance)
+        {
+            Target.BroadcastMessage("ApplyDamage", Damage, SendMessageOptions.DontRequireReceiver);
+            Target.BroadcastMessage("Direction", transform, SendMessageOptions.DontRequireReceiver);
+        }
     }
 }
