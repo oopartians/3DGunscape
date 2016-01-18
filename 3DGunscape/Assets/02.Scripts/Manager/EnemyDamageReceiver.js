3e1c6f1939c6d9db815c4c8c5344236f00069e13
Jin Park <jiny199109@gmail.com>
2016-01-18 18:25:00 +0900

    Use own Enemy Damage Receiver.

--- 3DGunscape/Assets/02.Scripts/Manager/EnemyDamageReceiver.js
+++ 3DGunscape/Assets/02.Scripts/Manager/EnemyDamageReceiver.js
@@ -41,6 +41,7 @@ function ApplyDamage(Arr : Object[]){
 
 		Invoke("DelayedDetonate", effectDelay);
 	}
+	BroadcastMessage ("GotHit", SendMessageOptions.DontRequireReceiver);
 }
 
 function ApplyDamagePlayer (damage : float){
@@ -66,6 +67,7 @@ function ApplyDamagePlayer (damage : float){
 
 		Invoke("DelayedDetonate", effectDelay);
 	}
+	BroadcastMessage ("GotHit", SendMessageOptions.DontRequireReceiver);
 }
 
 function ApplyDamage (damage : float){
@@ -86,6 +88,7 @@ function ApplyDamage (damage : float){
 
 		Invoke("DelayedDetonate", effectDelay);
 	}
+	BroadcastMessage ("GotHit", SendMessageOptions.DontRequireReceiver);
 }
 
 function DelayedDetonate(){
@@ -115,6 +118,7 @@ function Detonate(){
 		emitter.transform.parent = null;
 	}
 	BroadcastMessage ("Die", SendMessageOptions.DontRequireReceiver);
-	Destroy(gameObject);
+    // TODO: Destory object if there is no "Die()" method in the object
+    //	Destroy(gameObject);
 
 }
